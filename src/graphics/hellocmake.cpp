58233f64828655c0533a2b7cdd74d0453bef7c09
pdabrow5 <01158021@pw.edu.pl>
2022-12-06 18:50:18 +0100

    First commin in Graphics Branch. Created directory and required Cmake files to start using OpenGL API

---
+++ src/graphics/hellocmake.cpp
@@ -0,0 +1,10 @@
+#include "graphics/hellocmake.h"
+
+
+
+glm::vec3 GetVector(){
+
+    return glm::vec3(1.0f, 1.0f, 0.0f);
+};
+
+
---
+++ src/graphics/hellocmake.h
@@ -0,0 +1,9 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<iostream>
+
+
+glm::vec3 GetVector();
