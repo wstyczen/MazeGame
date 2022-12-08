16db22498032bf9e0d6c501de4dc7649e0090e0e
pdabrow5 <01158021@pw.edu.pl>
2022-12-08 02:44:44 +0100

    Added SolidFigure class, tested if Shader class can use .frag and .vert files

--- src/graphics/main_hello.cpp
+++ src/graphics/main_hello.cpp
@@ -1,13 +1,20 @@
 #include "graphics/hellocmake.h"
+#include "graphics/shapes/data_buffers/EBO.hpp"
+#include "graphics/shapes/data_buffers/VBO.hpp"
+#include "graphics/shapes/data_buffers/VAO.hpp"
+#include "graphics/shapes/solid_figure.hpp"
 #include<iostream>
 #include<cmath>
+#include<vector>
+#include<list>
+#include<iterator>
 
 
 const unsigned int width(1000), height(1000);
 
 // Vertices coordinates
 GLfloat vertices[] =
-{ //               COORDINATES              
+{ //               COORDINATES
 	-0.5f,  -0.5f, 0.5f,     0.8f, 0.3f,  0.02f,	// Lower left corner
 	-0.5f,  0.5f, 0.5f,     0.8f, 0.3f,  0.02f,	// Upper left corner
 	 0.5f,  0.5f, 0.5f,     0.8f, 0.3f,  0.02f,	// Upper right corner
@@ -21,21 +28,12 @@ GLfloat vertices[] =
 	 0.0f,  0.0f, 0.3f,		0.1f, 0.02f, 0.02f,// Dark inner point 1
 	-0.3f,  0.0f, 0.0f,		0.1f, 0.02f, 0.02f,	// Dark inner point 2
 	 0.0f,  0.0f, -0.3f,	0.1f, 0.02f, 0.02f,// Dark inner point 3
-	 0.3f,  0.0f, 0.0f,		0.1f, 0.02f, 0.02f,	// Dark inner point 4 
+	 0.3f,  0.0f, 0.0f,		0.1f, 0.02f, 0.02f,	// Dark inner point 4
 	 0.0f,  0.3f, 0.0f,		0.1f, 0.02f, 0.02f,	// Dark inner point 5
 	 0.0f,  -0.3f, 0.0f,	0.1f, 0.02f, 0.02f	// Dark inner point 6
 
 };
-GLfloat vertices2[] = 
-{
-	 -3.0f,  0.0f, 3.0f,     0.0f, 0.0f,  0.02f,	// Lower left corner
-	 -3.0f,  0.0f, -3.0f,     0.0f, 0.0f,  0.02f,	// Upper left corner
-	 3.0f,  0.0f, 3.0f,     0.0f, 0.0f,  0.02f,	// Upper right corner
-	 3.0f,  0.0f, -3.0f,     0.0f, 0.0f,  0.02f,	// Lower right corner
 
-};
-
-// Indices for vertices order
 GLuint indices[] =
 {
 
@@ -70,14 +68,6 @@ GLuint indices[] =
 	3, 0, 13
 
 };
-GLuint indices2[] =
-{
-
-	0, 1, 2,
-	3, 2, 1
-
-};
-
 
 
 int main()
@@ -85,19 +75,23 @@ int main()
 	// Initialize GLFW
 	glfwInit();
 
-	// Tell GLFW what version of OpenGL we are using 
+	// Tell GLFW what version of OpenGL we are using
 	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	// Tell GLFW we are using the CORE profile
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+  std::list<std::vector<GLfloat>> vector;
+  vector.push_back(std::vector<GLfloat>{1, 2, 3});
+  vector.push_back(std::vector<GLfloat>{4, 5, 6});
+  vector.push_back(std::vector<GLfloat>{7, 8, 9});
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
 	GLFWwindow* window = glfwCreateWindow(width, height, "HelloOpenGL", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
+
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -110,23 +104,31 @@ int main()
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
-
-
-
+  // std::ifstream myfile;
+  // myfile.open ("graphics/shaders/default.vert", std::ios::binary);
+  // std::string s;
+  // getline(myfile, s);
+  // std::cout << s << "\n\n\n\n dupaaaaa";
+ Shader shaderProgram("default.vert", "default.frag");
+ SolidFigure figure(vertices, sizeof(vertices), indices, sizeof(indices), {0.0f, 0.0f, -10.0f}, {0.0f, 0.0f, 0.0f});
 	// Generates Shader object using shaders defualt.vert and default.frag
-
+  for(std::list<std::vector<GLfloat>>::iterator i = vector.begin(); i != vector.end(); i++){
+    std::vector<GLfloat> a = *i;
+    std::cout<< a[0]<<", "<< a[1] << ", " << a[2]<<std::endl;
+  }
+  glEnable(GL_DEPTH_TEST);
 	while (!glfwWindowShouldClose(window))
 	{
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 0.5f);
 		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    shaderProgram.Activate();
+    figure.show(shaderProgram.GetId());
+
 		// Tell OpenGL which Shader Program we want to use
-
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
-	
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
@@ -135,7 +137,7 @@ int main()
 
 
 	// Delete all the objects we've created
-
+  shaderProgram.Delete();
 	glfwDestroyWindow(window);
 	// Terminate GLFW before ending the program
 	glfwTerminate();
---
+++ src/graphics/shapes/data_buffers/EBO.hpp
@@ -0,0 +1,24 @@
+#ifndef EBO_CLASS_H
+#define EBO_CLASS_H
+
+#include<glad/glad.h>
+
+class EBO
+{
+public:
+	// ID reference of Elements Buffer Object
+	GLuint ID;
+	unsigned int count;
+	// Constructor that generates a Elements Buffer Object and links it to indices
+	EBO(GLuint* indices, GLsizeiptr size);
+
+	// Binds the EBO
+	void Bind();
+	// Unbinds the EBO
+	void Unbind();
+	// Deletes the EBO
+	void Delete();
+	unsigned int GetCount();
+};
+
+#endif
