a0b1b373e16f97fcd70deb92aaa6ca81d0dbad26
pdabrow5 <01158021@pw.edu.pl>
2022-12-11 16:52:45 +0100

    Refactored dynamic figure class: has a method Act() that uses virtual method Perform() which computes current position of a figure.

--- src/graphics/shapes/complex_cube.hpp
+++ src/graphics/shapes/complex_cube.hpp
@@ -1,13 +1,27 @@
 #include "graphics/shapes/dynamic_solid_figure.hpp"
 
+
 class ComplexCube : public DynamicSolidFigure {
- public:
-  ComplexCube(const GLfloat side = 1,
-                    const glm::vec3 vertex_color = {0.0f, 0.0f, 0.0f},
-                    const glm::vec3 inner_color = {1.0f, 1.0f, 1.0f},
-                    const glm::vec3 posi = {0.0f, 0.0f, 0.0f},
-                    const glm::vec3 pos = {0.0f, 0.0f, 0.0f});
-  void Roll(const glm::vec2 &turn_vec, GLfloat scale);
- protected:
-  GLfloat size;
+  public:
+    enum MoveDirection {move_north, move_east, move_south, move_west, steady};
+    struct MoveSettings {
+      float acceleration;
+      float start_velocity;
+    };
+    ComplexCube(const GLfloat side = 1,
+                      const glm::vec3 vertex_color = {0.0f, 0.0f, 0.0f},
+                      const glm::vec3 inner_color = {1.0f, 1.0f, 1.0f},
+                      const glm::vec3 posi = {0.0f, 0.0f, 0.0f},
+                      const glm::vec3 pos = {0.0f, 0.0f, 0.0f});
+    void MakeMove(ComplexCube::MoveDirection direction);
+
+  protected:
+    void Roll(const glm::vec3 &turn_vec, GLfloat scale);
+    GLfloat size;
+    glm::vec3 start_position;
+    glm::vec3 ang_vel = {0.0f, 0.0f, 0.0f};
+    glm::vec3 lin_vel = {0.0f, 0.0f, 0.0f};
+    ComplexCube::MoveDirection move_state = steady;
+    ComplexCube::MoveSettings move_settings = {0.2f, 2.0f};
+    void Perform();
 };
