67d917118b55ac78df0928f1c94da46bcd756238
wstyczen <57729793+wstyczen@users.noreply.github.com>
2023-01-07 17:50:00 +0100

    Add Google Test as a submodule

--- build.sh
+++ build.sh
@@ -3,15 +3,26 @@ git submodule update --init
 if [[ ! -d "build" ]]; then mkdir build; fi
 cd build
 
-if [[ ! -d "debug" ]]; then mkdir debug; fi
-cd debug
-cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ../..
-cmake --build .
+release=false
 
-cd ..
+while getopts r name
+do
+  case ${name} in
+    r) release=true
+    ;;
+  esac
+done
 
-if [[ ! -d "release" ]]; then mkdir release; fi
-cd release
-cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ../..
-cmake --build .
+
+if [ $release == true ]; then
+  if [[ ! -d "release" ]]; then mkdir release; fi
+  cd release
+  cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ../..
+  cmake --build .
+else
+  if [[ ! -d "debug" ]]; then mkdir debug; fi
+  cd debug
+  cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ../..
+  cmake --build .
+fi
 
