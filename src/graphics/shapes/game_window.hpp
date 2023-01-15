6fbae2ddb5e89b31743460e0370449072b76977a
pdabrow5 <01158021@pw.edu.pl>
2023-01-15 22:13:52 +0100

    Add documentation part4

--- src/graphics/shapes/game_window.hpp
+++ src/graphics/shapes/game_window.hpp
@@ -11,31 +11,119 @@
 #include "maze/solvers/solver.hpp"
 #include "maze/solvers/solver_factory.hpp"
 namespace graphics {
-
+/**
+ * @brief Class that combines figures to create window with visualisation of a MazeGame.
+ *
+ */
 class GameWindow {
  public:
+  /**
+  * @brief Construct a new Game Window object
+  *
+  * @param maze maze to be visualised
+  * @param cube_position starting position of a cube
+  * @param goal destinate position of a cube
+  */
   GameWindow(const maze::Layout& maze,
              const maze::Cell& cube_position,
              const maze::Cell& goal);
+  /**
+   * @brief Updates current state of each figure in window
+   */
   void Act();
+  /**
+   * @brief Displays all figures in window
+   *
+   */
   void Show() const;
+  /**
+   * @brief Starts maze Appear() animation
+   *
+   */
   void LiftMaze();
+  /**
+   * @brief Starts maze Disappear() animation
+   *
+   */
   void DropMaze();
-
+  /**
+   * @brief Add taken path describing curently visited cells in order to display it
+   *
+   * @param maze maze in which path was taken
+   * @param taken_path taken path
+   */
   void AddTakenPath(const maze::Layout& maze,
-                    const std::vector<maze::Position>& solution);
+                    const std::vector<maze::Position>& taken_path);
+  /**
+   * @brief Deletes taken path so it won't be displayed anymore
+   *
+   */
   void ResetTakenPath(){if(taken_path_)taken_path_.reset();}
+  /**
+   * @brief Add current solving path to be displayed
+   *
+   * @param maze maze which solution is given path
+   * @param solution solution path
+   */
   void AddSolvingPath(const maze::Layout& maze,
                       const std::vector<maze::Position>& solution);
+  /**
+   * @brief Deletes solving path so it won't be displayed anymore
+   *
+   */
   void ResetSolvingPath(){if(solving_path_)solving_path_.reset();}
+  /**
+   * @brief Deletes destination tile so it won't be displayed anymore
+   *
+   */
   void ResetDestination(){if(destination_)destination_.reset();}
-
+  /**
+   * @brief Checks if cube is moving
+   *
+   * @return true cube is moving
+   * @return false cube is standing still
+   */
   bool IsCubeMoving() const;
+  /**
+   * @brief Waits until cube will finish rolling animaton
+   *
+   */
   void WaitForCubeMoveToComplete();
+  /**
+   * @brief Moves Cube in diretion
+   *
+   * @param direction direction of Cube's move
+   * @return true if Cube was standing still till method wasn't used
+   * @return false if Cube is currently moving
+   */
   bool MoveCube(const ComplexCube::FigureState& direction);
+  /**
+   * @brief Destroy the Game Window object,
+   * makes sure that every figure object and
+   * data in buffers related to them are deleted.
+   */
   ~GameWindow();
+  /**
+   * @brief returns value of glfwWindowShouldClose() of window that is displayed
+   *
+   * @return true if user tried to close window
+   * @return false if window should keep being displayed
+   */
   bool WindowShouldClose() const { return glfwWindowShouldClose(window_); }
+  /**
+   * @brief Get the current state of given keyboard key while using window
+   *
+   * @param key Keyboard key to be checked
+   * @return One of `GLFW_PRESS` or `GLFW_RELEASE`.
+   */
   int GetKeyState(const int& key) const { return glfwGetKey(window_, key); }
+  /**
+   * @brief Creates figures in window needed to play a MazeGame
+   *
+   * @param maze maze to display
+   * @param cube_position cube start position
+   * @param goal cube destinate position
+   */
   void InitFigures(const maze::Layout& maze,
                    const maze::Cell& cube_position,
                    const maze::Cell& goal);
--- src/graphics/shapes/maze_figure.hpp
+++ src/graphics/shapes/maze_figure.hpp
@@ -4,40 +4,98 @@
 #include "maze/layout.hpp"
 
 namespace graphics {
-
+/**
+ * @brief Contains all needed information to create a shape.
+ */
 struct FigureVertexData {
   std::vector<GLfloat> vertices;
   std::vector<GLuint> indices;
 };
-
+/**
+ * @brief Figure in shape of a maze.
+ *
+ */
 class MazeFigure : public DynamicSolidFigure {
  public:
+  /**
+   * @brief Describes current state of a MazeFigure
+   */
   enum FigureState { appear, disappear, steady };
+  /**
+   * @brief Settings of a MazeFigure that contain information about it's movement
+   *
+   */
   struct MoveSettings {
+    //acceleration of a MazeFigure while moving
     float acceleration;
+    //start velocity of a MazeFigure when triggered by Disappear() or Appear() method
     float start_velocity;
   };
+  /**
+   * @brief Contains all settings desribing apearance and behaviour of a MazeFigure
+   *
+   */
   struct MazeSettings{
+    //height of a MazeFigure
     GLfloat height;
+    //Size of a grid on which walls of a maze are placed
     GLfloat cell_size;
+    //lenght of base edge of each wall in maze which are cuboids with square base
     GLfloat side_of_a_base;
+    //color of MazeFigure, values should be in [0,1]
     glm::vec3 color;
+    //shading of a vertical sides of a wall, values should be in [0,1]
     glm::vec2 shading;
+    //settings defines movement speed
     MoveSettings move_settings;
   };
-
+  /**
+   * @brief Construct a new Maze Figure object
+   *
+   * @param maze vector of coordinates of walls in maze, around each coordinate wall will be placed
+   * @param posi starting position
+   * @param pos starting pose
+   * @param maze_settings settings of a maze
+   */
   MazeFigure(const std::vector<glm::vec2>& maze,
             const glm::vec3 &posi,
             const glm::vec3 &pos,
             const MazeSettings &maze_settings);
-
+  /**
+   * @brief Construct a new Maze Figure from DynamicSolidFigure, usable to expand DynamicSolidFigure functionality
+   *
+   * @param maze_figure figure to enhance
+   * @param maze_settings settings of new MazeFigure object
+   */
   MazeFigure(const DynamicSolidFigure &maze_figure,
               const MazeSettings &maze_settings);
 
-
+  /**
+   * @brief Triggers MazeFigure to appear
+   *
+   */
   void Appear();
+  /**
+   * @brief Triggers MazeFigure to disappear
+   *
+   */
   void Disappear();
+  /**
+   * @brief Check if MazeFigure is currently moving
+   *
+   * @return true if state isn't equal MazeFigure::FigureState::steady
+   * @return else false
+   */
   bool IsMoving();
+  /**
+   * @brief Describes MazeFigure's behaviour as state machine depending on move_state_ value. \n
+   * States of maze: \n
+   * -steady - maze is not moving, \n
+   * -appear - maze will lift until it approaches it's beggining position, \n
+   * -disappear - maze will lower itself until it disappears \n
+   *
+   * Speed of movement is described by maze_settings_
+   */
   void Act();
   static std::vector<glm::vec2> Path2Vec(const maze::Layout& maze,
                                          const std::vector<maze::Cell> path);
@@ -46,24 +104,44 @@ class MazeFigure : public DynamicSolidFigure {
   @return Vector of coordinates of walls that maze is made of.
   */
   static std::vector<glm::vec2> Layout2VecOfWalls(const maze::Layout* maze);
-
+  /**
+   * @brief Creates DynamicSolidFigure as construction of walls placed in given coordinates
+   *
+   * @param maze_walls coordinates in which walls will be placed.
+   * @param wall_model 3D model of a wall described as FigureVertexData object
+   * @param posi starting position
+   * @param pos starting pose
+   * @param cell_size size of a grid on which walls of a maze are placed
+   * @return DynamicSolidFigure in shape of a maze
+   */
   static DynamicSolidFigure VectorToMapFigure(
       const std::vector<glm::vec2>& maze_walls,
       const FigureVertexData& wall_model,
       const glm::vec3& posi,
       const glm::vec3& pos,
-      const GLfloat& side_of_a_base);
-
+      const GLfloat& cell_size);
+  /**
+   * @brief Creates FigureVertexData object describing a cuboid with a square base, cuboid has no floor in order to reduce data
+   *
+   * @param height height of a cuboid
+   * @param side_of_a_base side of a square base
+   * @param celling_color color of the top side
+   * @param wall_shading the way that top color changes towards down
+   * @return FigureVertexData describing cuboid
+   */
   static FigureVertexData GetWallTemplate(const GLfloat& height,
                                           const GLfloat& side_of_a_base,
                                           const glm::vec3& celling_color,
                                           const glm::vec2& wall_shading);
 
  protected:
+  //settings of a MazeFigure
   MazeSettings maze_settings_;
+  //Start position of a MazeFigure
   glm::vec3 start_position_;
-  glm::vec3 ang_vel_ = {0.0f, 0.0f, 0.0f};
+  //current velocity of a MazeFigure
   glm::vec3 lin_vel_ = {0.0f, 0.0f, 0.0f};
+  //Current state of a MazeFigure
   MazeFigure::FigureState move_state_ = steady;
 };
 }  // namespace graphics
